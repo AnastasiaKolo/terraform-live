ab6146a5e27a87a69934c2d5453691d8f21c1e9b
AnastasiaKolo <kolodochko@gmail.com>
2020-11-17 20:48:05 +0300

    loops in terraform. chapter 5

---
+++ global/iam/main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+#resource "aws_iam_user" "example" {
+#  count = length(var.user_names)
+#  name  = var.user_names[count.index]
+#}
+#output "all_arns" {
+#  value       = aws_iam_user.example[*].arn
+#  description = "The ARNs for all users"
+#}
+
+resource "aws_iam_user" "example" {
+  for_each = toset(var.user_names)
+  name     = each.value
+}
+
+output "all_users" {
+  value = aws_iam_user.example
+}
+
+
