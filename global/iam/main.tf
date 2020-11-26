b57b8fd9d60255aac9f7e3bcae7a9726bbaa4d51
AnastasiaKolo <kolodochko@gmail.com>
2020-11-26 20:53:26 +0300

    5 chapter

--- global/iam/main.tf
+++ global/iam/main.tf
@@ -16,8 +16,7 @@ resource "aws_iam_user" "example" {
   name     = each.value
 }
 
-output "all_users" {
-  value = aws_iam_user.example
+output "all_arns" {
+  value = values(aws_iam_user.example)[*].arn
 }
-
 
