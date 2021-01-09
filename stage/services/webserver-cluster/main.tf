4eb6a97af5cf300ba3b8fb14e0c9a1e6d6d3fec7
AnastasiaKolo <kolodochko@gmail.com>
2021-01-09 21:10:15 +0300

    chapter 5 zero-downtime

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -3,8 +3,11 @@ provider "aws" {
 }
 
 module "webserver_cluster" {
-  source = "github.com/AnastasiaKolo/terraform-modules//services/webserver-cluster?ref=v0.0.5"
+  source = "github.com/AnastasiaKolo/terraform-modules//services/webserver-cluster?ref=v0.0.7"
 
+  ami         = "ami-0c55b159cbfafe1f0"
+  server_text = "New server text"
+  
   cluster_name           = "webservers-stage"
   db_remote_state_bucket = "terraform-up-and-running-state-12345"
   db_remote_state_key    = "stage/data-stores/mysql/terraform.tfstate"
@@ -13,7 +16,6 @@ module "webserver_cluster" {
   min_size      = 2
   max_size      = 2
   enable_autoscaling   = false
-  enable_new_user_data = true
 }
 
 resource "aws_security_group_rule" "allow_testing_inbound" {
